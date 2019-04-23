f68b59eadaabf16255be12eaad5e113efab0a975
tomotagwork <tomotagwork@gmail.com>
2019-04-23 09:44:36 +0900

    fix

--- Linux_Resources/ShellScript/sshGetDaSysJson_eplexa.sh
+++ Linux_Resources/ShellScript/sshGetDaSysJson_eplexa.sh
@@ -4,6 +4,6 @@ outputDir=/var/log/zos_da_json/
 yyyymmdd=$(date '+%Y%m%d')
 outputFile=${outputDir}da_eplexa_${yyyymmdd}.txt
 
-ssh CICS004@9.188.216.204 "REXX/getDaSysJobJson.sh"  >> ${outputFile}
+ssh CICS004@eplexa "REXX/getDaSysJobJson.sh"  >> ${outputFile}
 
 
